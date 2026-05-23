37b1bc088a0328e1d53410de5688a544dcec7977
ArafathUIU <arafathakash77@gmail.com>
2026-05-23 10:50:07 +0600

    [7.1] Plugin: Add package init with PluginLoader and PluginRegistry exports

--- codeforge/plugins/__init__.py
+++ codeforge/plugins/__init__.py
@@ -1,1 +1,14 @@
-"""Plugin system for extending CodeForge with custom agents and hooks."""
+"""Plugin system for CodeForge.
+
+Enables custom agent plugins with discovery, loading, and
+registration support for extending the multi-agent system.
+"""
+
+from codeforge.plugins.loader import PluginLoader, PluginInfo
+from codeforge.plugins.registry import PluginRegistry
+
+__all__ = [
+    "PluginInfo",
+    "PluginLoader",
+    "PluginRegistry",
+]
